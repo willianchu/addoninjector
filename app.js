ed2f3a3084a610dc00ac3ec66987efbc334aed48
willianchu <willianchu@hotmail.com>
2023-06-01 10:55:38 -0700

    personalized websites alerts

--- app.js
+++ app.js
@@ -3,3 +3,23 @@
 const site = window.location.hostname;
 
 alert("Injected from " + site + " to " + window.location.hostname + "!")
+
+// JS codes for youtube
+if (site == "www.youtube.com") {
+  alert("This is youtube!")
+}
+
+// JS codes for google
+if (site == "www.google.com") {
+  alert("This is google!")
+}
+
+// JS codes for microsoft
+if (site == "www.microsoft.com") {
+  alert("This is microsoft!")
+}
+
+// JS codes for homedepot.ca
+if (site == "www.homedepot.ca") {
+  alert("This is Home Depot Canada!")
+}
