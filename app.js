496d9d9e742b7ca495d41dbdc7526c704863b0c4
willianchu <willianchu@hotmail.com>
2023-06-02 21:31:45 -0700

    update listener for a background script

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 // Add on Test
 // Get the URL
 const site = window.location.hostname;
+// Add a Javascript codebar Generator library into the website head tag
+const Add_Javascript_Codebar_Generator = () => document.head.appendChild(document.createElement("script")).src = "https://cdnjs.cloudflare.com/ajax/libs/jsbarcode/3.11.3/JsBarcode.all.min.js";
+
+// Find and transform SKUs into barcodes
+function transformSKUsIntoBarcodes() {
+  const skuElements = document.querySelectorAll(":contains('SKU')");
+
+  skuElements.forEach((element) => {
+    const text = element.textContent;
+    const regex = /SKU\s+#\s+(\d+)/;
+    const match = text.match(regex);
+
+    if (match) {
+      const skuNumber = match[1];
+      const barcodeImageURL = generateBarcode(skuNumber);
+
+      // Replace the SKU text with the barcode image
+      const barcodeImageElement = document.createElement("img");
+      barcodeImageElement.src = barcodeImageURL;
+      element.textContent = "";
+      element.appendChild(barcodeImageElement);
+    }
+  });
+}
 
 // Add Custom CSS - Function style inside the website head tag
 const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
@@ -128,10 +152,20 @@ if (site == "www.google.com") {
 
 // JS codes for microsoft
 if (site == "www.microsoft.com") {
-  alert("This is microsoft!")
+  Add_Custom_Style(`
+    * {
+      filter: invert(1) !important;
+    }
+  `);	
 }
 
 // JS codes for homedepot.ca
 if (site == "www.homedepot.ca") {
-  alert("This is Home Depot Canada!")
+  document.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete' && tab.active) {
+      console.log("Transforming SKU numbers into barcodes...");
+      transformSKUsIntoBarcodes();
+  
+    }
+  })
 }
